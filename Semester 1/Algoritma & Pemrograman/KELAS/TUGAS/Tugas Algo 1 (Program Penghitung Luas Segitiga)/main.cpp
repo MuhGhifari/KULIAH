5d113c226ff5a736b1fed098a83e30f26df5e5d2
MuhGhifari <muhammadghifari37@gmail.com>
2023-10-04 20:53:56 +0700

    add comments

--- Semester 1/Algoritma & Pemrograman/KELAS/TUGAS/Tugas Algo 1 (Program Penghitung Luas Segitiga)/main.cpp
+++ Semester 1/Algoritma & Pemrograman/KELAS/TUGAS/Tugas Algo 1 (Program Penghitung Luas Segitiga)/main.cpp
@@ -1,45 +1,50 @@
-// PROJECT     : 01
-// PROGRAM     : MENGHITUNG LUAS SEGITIGA
-// AUTHOR      : MUHAMMAD GHIFARI
-// NPM         : 065123020
-// TANGGAL     : 28/09/2023
-
-#include <iostream>
-#include <fstream>
+#include <iostream> // library yang memberi program fungsi input/output
+#include <fstream> // library yang memberi program akses read/write file 
 
 using namespace std;
 
 int main() {
+    // deklarasi variabel
     char str[9];
-    float alas, tinggi, luas;
+    float alas, tinggi, luas = 0.0;
     
-    luas = 0.0;
+    // read file 01_input.txt
+    ifstream file_in ("01_input.txt"); // file_in itu seperti variabel kelas ifstream
     
-    ifstream file_in ("01_input.txt");
-    
-    if(!file_in) {
+    if(!file_in) { // jika file gagal dibaca
         cout << "INPUT FILE TIDAK BISA DIBUKA, ERROR, PROGRAM DIHENTIKAN" << endl;
         exit(-1);
     }
-    else {
+    else { // jika file berhasil dibaca
+        // membaca baris ke 2 dari file 01_input.txt dan memasukkannya ke dalam variabel str
         file_in.getline(str, 9, '\n');
         file_in.getline(str, 9, '\n');
-        alas = atof(str);
+
+        // masukkan nilai str ke dalam variabel alas
+        alas = atof(str); // atof() untuk konversi nilai karakter ke numerik
         
+        // membaca baris ke 4 dari file 01_input.txt dan memasukkannya ke dalam variabel str
         file_in.getline(str, 9, '\n');
         file_in.getline(str, 9, '\n');
-        tinggi = atof(str);
+
+        // masukkan nilai str ke dalam variabel tinggi
+        tinggi = atof(str); // atof() untuk konversi nilai karakter ke numerik
     }
     
+    // rumus aritmatik mencari luas segitiga
     luas = 0.5 * alas * tinggi;
+    
+    // deklarasi variabel kelas ofstream
     ofstream file_out;
+
+    // buat/edit file 01_output.txt
     file_out.open("01_output.txt");
     
-    if(!file_out) {
+    if(!file_out) { // jika file gagal dibuat
         cout << "OUTPUT FILE TIDAK BISA DIBUKA, ERROR, PROGRAM DIHENTIKAN" << endl;
         exit(-1);
     }
-    else {
+    else { // jika file berhasil dibuat
         file_out << "program selesai dieksekusi" << endl;
         file_out << "Hasil Eksekusi Program adalah sebagai berikut : " << endl;
         file_out << "-------------------------------------------------" << endl;
@@ -50,6 +55,7 @@ int main() {
         file_out << "Luas Segitiga      = " << luas << endl;
     }
     
+    // tutup akses ke file 01_input.txt & 01_output.txt
     file_in.close();
     file_out.close();
     
