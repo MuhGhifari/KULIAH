87ee080a36ab7c8e2d14043f4baca909da524aea
MuhGhifari <muhammadghifari37@gmail.com>
2023-10-30 14:05:34 +0700

    fix 1 quiz P6 (medium)

--- Semester 1/Algoritma & Pemrograman/Praktikum/P6/Quiz/medium.cpp
+++ Semester 1/Algoritma & Pemrograman/Praktikum/P6/Quiz/medium.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main() {
   int input;
+  char ulang;
 
   cout << "-------- PROGRAM PENENTU BILANGAN GANJIL/GENAP --------\n";
+  start:
   cout << "Masukkan angka : "; cin >> input;
 
   if (input % 2 == 0) {
@@ -13,5 +15,16 @@ int main() {
   } else {
     cout << "Bilangan tersebut adalah angka ganjil!" << endl;
   }
-  return 0;
+
+  cout << "-------------------------------------------------------\n";
+  cout << "Apakah anda ingin menginput kembali? (Y/N) : "; cin >> ulang;
+  if (tolower(ulang) == 'y') {
+    goto start;
+  } else if (tolower(ulang) == 'n') {
+    cout << "Terima kasih..." << endl;
+    return 0;
+  } else {
+    cout << "Input error, program dimatikan" << endl;
+  }
+
 }
