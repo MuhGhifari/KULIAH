291a7ce72a635439d110896343d40757c7fe34ad
MuhGhifari <muhammadghifari37@gmail.com>
2023-11-28 13:50:04 +0700

    update algo 4

--- Semester 1/Algoritma & Pemrograman/Teori/TUGAS/Tugas Algo 4 (Program looping rerata)/main.cpp
+++ Semester 1/Algoritma & Pemrograman/Teori/TUGAS/Tugas Algo 4 (Program looping rerata)/main.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-	int n, i = 1;
-	float total = 0.0;
+	ifstream input("input.txt");
+	ofstream output;
+	output.open("output.txt");
+	int n = 0;
+	string str;
+	double total = 0;
 
-	cout << "Masukkan Jumlah Bilangan : ";
-	cin >> n;
-	cout << endl;
-
-	while (i <= n) {
-		float bilangan;
-		cout << "Masukkan bilangan ke-" << i << " : ";
-		cin >> bilangan;
-		total = total + bilangan;
-		i++;
+	output << "================ PROGRAM PENGHITUNG RATA-RATA ================" << endl;
+	output << "Bilangan : [ ";
+	while (getline(input,str)) {
+		++n;
+		total = total + stod(str);
+		if (n == 1) {
+			output << str;
+		} else {
+			output << ", " << str;
+		}
 	}
-
+	output << " ]" << endl;
+	output << "==============================================================" << endl;
 	total = total / n;
-	cout << "\nHasil rata-rata dari bilangan-bilangan tersebut adalah : " << total << endl;
+	output << "Maka rata-rata dari bilangan-bilangan tersebut adalah : " << total << endl;
 	return 0;
 }
