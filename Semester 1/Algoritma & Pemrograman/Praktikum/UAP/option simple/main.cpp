e7f2e157bcfbe4afcc5444c41d766558a62d0bff
MuhGhifari <muhammadghifari37@gmail.com>
2023-12-16 12:09:22 +0700

    tambah catch error

--- Semester 1/Algoritma & Pemrograman/Praktikum/UAP/option simple/main.cpp
+++ Semester 1/Algoritma & Pemrograman/Praktikum/UAP/option simple/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <sstream>
 
 using namespace std;
@@ -16,9 +17,20 @@ int main() {
   const int pecahanRupiah[] = {100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100};
   int n = sizeof(pecahanRupiah) / sizeof(pecahanRupiah[0]);
   vector<int> banyakPecahan(n, 0);
+  string str;
   int tunai;
+  char ulang;
   cout <<"\x1b[0m======================\x1b[34m Program Penghitung Pecahan Rupiah \x1b[0m======================" << endl;
-  cout << "\x1b[32mMasukkan total uang : \x1b[0m"; cin >> tunai;
+  input:
+  cout << "\x1b[32mMasukkan total uang : \x1b[0m"; cin >> str;
+  try {
+    tunai = stoi(str);
+  }
+  catch(const exception& e) {
+    cerr << "\x1b[31m[ERROR] : Input harus berbentuk angka!\033[0m\x1b[0m" << endl;
+    cerr << "\x1b[33mSilahkan coba kembali...\x1b[0m" << endl;
+    goto input;
+  }
 
   for (int i = 0; i < n; i++) {
     while (tunai >= pecahanRupiah[i]) {
@@ -27,10 +39,27 @@ int main() {
     }
   }
   
-  cout << "\x1b[0m--------------------------------------------" << endl;
+  cout << "\x1b[0m-------------------------------------------------------------------------------" << endl;
+  cout << "Pecahan uang yang dibutuhkan :" << endl;
   for (int i = 0; i < banyakPecahan.size(); i++) {
     if (banyakPecahan[i] > 0) {
       cout << "\x1b[0m(\x1b[35m" << banyakPecahan[i] << "x\x1b[0m) \x1b[36m" << formatRupiah(pecahanRupiah[i]) << endl;
     }
   }
+
+  konfirmasi_selesai:
+  cout << "\x1b[0m-------------------------------------------------------------------------------" << endl;
+  cout << "\x1b[32mApakah anda ingin input ulang? (Y/N) : \x1b[0m"; cin >> ulang;
+  if (tolower(ulang) == 'y') {
+    cout << "\x1b[0m-------------------------------------------------------------------------------" << endl;
+    goto input;
+  } else if (tolower(ulang) == 'n') {
+    cout << "Terima kasih..." << endl;
+  } else {
+    cerr << "\x1b[31m[ERROR] : Input harus huruf y atau n!\x1b[0m" << endl;
+    cerr << "\x1b[33mSilahkan coba kembali...\x1b[0m" << endl;
+    goto konfirmasi_selesai;
+  }
+  
+  return 0;
 }
